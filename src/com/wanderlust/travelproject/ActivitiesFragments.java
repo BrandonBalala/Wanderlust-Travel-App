54c07bed475d102e3dafe2c45905ca4a8d209892
theMarvin <theMarvin@10.230.34.242>
2015-11-25 10:49:41 -0500

    test push

--- src/com/wanderlust/travelproject/ActivitiesFragments.java
+++ src/com/wanderlust/travelproject/ActivitiesFragments.java
@@ -19,6 +19,101 @@ public class ActivitiesFragments extends Fragment {
 	View view_a;
 
 	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		Log.d(TAG, "FRAGMENT onActivityCreated(): " + getShownIndex());
+		super.onActivityCreated(savedInstanceState);
+
+		String[] tripTypes = getResources().getStringArray(R.array.trip_types);
+		// Receive intent index of the current trips displayed.
+		// Receive intent because the call is from a portrait view of the device
+		Intent intent = getActivity().getIntent();
+		int index = intent.getIntExtra("index", -1);
+
+		// use of arguments is used when the call to the fragment is from a
+		// landscape view.
+		Bundle args = getArguments();
+		if (index == -1)
+			index = args.getInt("index", -1);
+
+		view_a = getView();
+		
+		
+		LinearLayout ll = new LinearLayout(getActivity());
+		TextView tv = new TextView(getActivity());
+
+		// View separator = new View(this.getActivity());
+		// separator.setMinimumWidth(ll.getWidth());
+		// separator.setMinimumHeight(ll.getHeight());
+		// separator.setBackgroundColor(color.darker_gray);
+
+		tv.setText("You are going for a " + tripTypes[index] + " :");
+		ll.addView(tv);
+
+		// ll.addView(separator);
+
+		LinearLayout trips = new LinearLayout(getActivity());
+		trips.setOrientation(LinearLayout.VERTICAL);
+
+		switch (index) {
+		case 0:
+			for (int i = 0; i <= 2; i++)
+				trips.addView(addtextView(0, i));
+			break;
+		case 1:
+			for (int i = 0; i <= 1; i++)
+				trips.addView(addtextView(0, i));
+			break;
+		case 2:
+			for (int i = 0; i <= 1; i++)
+				trips.addView(addtextView(0, i));
+			break;
+		case 3:
+			for (int i = 0; i <= 1; i++)
+				trips.addView(addtextView(0, i));
+			break;
+		default:
+			for (int i = 0; i <= 1; i++)
+				trips.addView(addtextView(0, i));
+			break;
+		}
+
+		ll.addView(trips);
+		view_a = ll;
+		
+		// // Load the URL after the Webview has been created in onCreateView()
+		// String[] fieldNoteAnimals = getResources().getStringArray(
+		// R.array.fieldnotes_array);
+		// //R.array.fieldnoteurls_array);
+		// int fieldNoteAnimalIx = getShownIndex();
+		//
+		// WebView webview = getWebView();
+		// webview.setPadding(2, 2, 2, 2);
+		// webview.getSettings().setLoadWithOverviewMode(true);
+		// webview.getSettings().setUseWideViewPort(true);
+		// // PMC was loading in browser, added this to load in webview
+		// webview.setWebViewClient(new WebViewClient());
+		//
+		// /*
+		// * PMC hardware accelerated canvas rendering is not supported in
+		// Chromium WebView.
+		// * so turn off hardware accelerated canvas: (layer = software, not
+		// devfault hardware
+		// */
+		// webview.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+		//
+		// if (fieldNoteAnimalIx != -1) {
+		// webview.loadUrl(url+fieldNoteAnimals[fieldNoteAnimalIx]);
+		// //String fieldNoteUrl = fieldNoteUrls[fieldNoteUrlIndex];
+		// //webview.loadUrl(fieldNoteUrl);
+		// }
+		// else
+		// {
+		// webview.loadUrl(url);
+		// }
+
+	}
+
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		String[] tripTypes = getResources().getStringArray(R.array.trip_types);
 		// Receive intent index of the current trips displayed.
@@ -44,18 +139,17 @@ public class ActivitiesFragments extends Fragment {
 
 		LinearLayout ll = new LinearLayout(getActivity());
 		TextView tv = new TextView(getActivity());
-		
-//		View separator = new View(this.getActivity());
-//		separator.setMinimumWidth(ll.getWidth());
-//		separator.setMinimumHeight(ll.getHeight());
-//		separator.setBackgroundColor(color.darker_gray);
-		
+
+		// View separator = new View(this.getActivity());
+		// separator.setMinimumWidth(ll.getWidth());
+		// separator.setMinimumHeight(ll.getHeight());
+		// separator.setBackgroundColor(color.darker_gray);
+
 		tv.setText("You are going for a " + tripTypes[index] + " :");
 		ll.addView(tv);
-		
-//		ll.addView(separator);
-		
-		
+
+		// ll.addView(separator);
+
 		LinearLayout trips = new LinearLayout(getActivity());
 		trips.setOrientation(LinearLayout.VERTICAL);
 
@@ -93,15 +187,15 @@ public class ActivitiesFragments extends Fragment {
 
 		String[] walktripsdesc = { "Long Walk", "short walks", "Lazy walk" };
 		String[] walktripcode = { "US", "PH", "CN" };
-		String[] walktripcity = { "New York", "Manila", "Montreal" };
+		String[] walktripcity = { "New Yoradadadddadsdssdk", "Manila", "Montreal" };
 
 		String[] flytripsdesc = { "33 Hours flight", "5 hour flight" };
-		String[] flytripcode = { "PH", "US" };
+		String[] flytripcode = { "PH", "USasdsadsadd" };
 		String[] flytripcity = { "Manila", "New York" };
 
 		String[] boattripsdesc = { "Cross the river", "Cruise" };
 		String[] boattripcode = { "PH", "CN" };
-		String[] boattripcity = { "Manila", "Montreal" };
+		String[] boattripcity = { "Manila", "Monasdadadsdtreal" };
 
 		String[] ferrytripsdesc = { "Staten Island crossing", "US - Canada Border" };
 		String[] ferrytripcode = { "US", "CN" };
